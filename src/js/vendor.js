1d47f2d3da2c8c9f7ec2a7b3d2381d268ad17153
Rey Ramos <reymundo.ramos@gmail.com>
2017-02-12 23:16:13 -0500

    added IE11 Support

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,10 +1,7 @@
 
 
 //polyfills for IE
-// require("babel-polyfill/dist/polyfill");
-// require('es6-promise/dist/es6-promise');
 require('es6-shim/es6-shim');
-// require('core-js/es7/reflect');
 
 require('angular');
 require('angular-ui-router/release/angular-ui-router');
